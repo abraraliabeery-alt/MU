44c76a65dcf65e4a0e101ec1dd66d4f1c6f7d3fd
Abrar-Ali123 <82758512+Abrar-Ali123@users.noreply.github.com>
2026-05-04 15:56:21 +0300

    update project

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -11,12 +11,14 @@
     <meta property="og:description" content="@yield('og_description', '')">
     <meta property="og:type" content="website">
     <meta property="og:locale" content="{{ app()->getLocale() === 'ar' ? 'ar_SA' : 'en_US' }}">
-    <meta property="og:image" content="@yield('og_image', asset('images/image (3).png'))">
+    <meta property="og:image" content="/images/image.png">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=plus-jakarta-sans:400,500,600,700|cairo:400,600,700" rel="stylesheet" />
 
-    <link rel="icon" type="image/png" href="{{ asset('images/image (3).png') }}">
+    <link rel="icon" href="/favicon.ico?v=1">
+    <link rel="shortcut icon" href="/favicon.ico?v=1">
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/image.png?v=1">
 
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
